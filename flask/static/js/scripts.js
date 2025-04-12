ae5087ad7c398c0ad5b1452030980bfc6250896e
Дмитрий Коротков <dmitrii.korotkov@lemanapro.ru>
2025-04-12 21:07:52 +0300

    Сортировка заработала для всех столбцов кроме первого

--- flask/static/js/scripts.js
+++ flask/static/js/scripts.js
@@ -1,20 +1,42 @@
+<script>
 $(document).ready(function() {
     // Получаем данные через API
     $.getJSON('/api/data', function(data) {
-        // Преобразуем оценки в таблицу
         var experts = data.experts;
         var nominants = data.nominants;
         var votes = data.votes;
+        var nominantStats = data.nominant_stats;
 
-        // Перебираем все оценки и вставляем их в таблицу
+        // Заполнение оценок в таблице
         votes.forEach(function(vote) {
             var expertId = vote[0];
             var nominantId = vote[1];
             var rating = vote[2];
-            
-            // Ищем соответствующую ячейку в таблице
-            var cellId = "#rating-" + nominantId + "-" + expertId;
-            $(cellId).text(rating);
+
+            $("#rating-" + nominantId + "-" + expertId).text(rating);
+        });
+
+        // Заполнение средней оценки и дисперсии для каждого номинанта
+        nominants.forEach(function(nominant) {
+            var nominantId = nominant[0];
+            var avgRating = nominantStats[nominantId].avg.toFixed(2);
+            var variance = nominantStats[nominantId].variance.toFixed(2);
+
+            $("#avg-" + nominantId).text(avgRating);
+            $("#variance-" + nominantId).text(variance);
+        });
+
+        // Теперь, когда все данные загружены, инициализируем DataTables
+        $('#ratings-table').DataTable({
+            "paging": true,
+            "ordering": true,
+            "info": false,
+            "searching": false,
+            "autoWidth": false,
+            "columnDefs": [
+                { "type": "num", "targets": "_all" }
+            ]
         });
     });
 });
+</script>
