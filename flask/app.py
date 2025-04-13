12b2491833b28357545d596f05d176108ca22e55
Дмитрий Коротков <dmitrii.korotkov@lemanapro.ru>
2025-04-13 20:11:12 +0300

    Добавил поля "КПД" и "Цена за кг"

--- flask/app.py
+++ flask/app.py
@@ -13,8 +13,8 @@ def get_data():
     cursor.execute("SELECT id, name FROM experts")
     experts = cursor.fetchall()
 
-    # Извлекаем всех номинантов (secret_name вместо name)
-    cursor.execute("SELECT id, secret_name FROM nominants")
+    # Извлекаем всех номинантов (secret_name, price, grams)
+    cursor.execute("SELECT id, secret_name, price, grams FROM nominants")
     nominants = cursor.fetchall()
 
     # Извлекаем все оценки
@@ -36,18 +36,44 @@ def get_data():
         else:
             nominant_stats[nominant_id] = {'avg': 0, 'variance': 0}
 
+    # Добавляем вычисления "Цена за кг" и "КПД"
+    nominant_details = []
+    for nominant in nominants:
+        nominant_id = nominant[0]
+        price = nominant[2]
+        grams = nominant[3]
+        
+        # Вычисление "Цены за кг"
+        price_per_kg = (price / grams) * 1000 if grams != 0 else 0
+        
+        # Вычисление КПД (средняя оценка / цена за кг)
+        avg_rating = nominant_stats.get(nominant_id, {}).get('avg', 0)
+        kpd = avg_rating / price_per_kg if price_per_kg != 0 else 0
+        
+        nominant_details.append({
+            'id': nominant_id,
+            'secret_name': nominant[1],
+            'price_per_kg': price_per_kg,
+            'kpd': kpd
+        })
+
     conn.close()
-    return experts, nominants, votes, nominant_stats
+    return experts, nominants, votes, nominant_stats, nominant_details
+
 
 @app.route('/')
 def index():
-    experts, nominants, votes, nominant_stats = get_data()
-    return render_template('index.html', experts=experts, nominants=nominants, votes=votes, nominant_stats=nominant_stats)
+    experts, nominants, votes, nominant_stats, nominant_details = get_data()
+    return render_template('index.html', experts=experts, nominants=nominants, votes=votes, 
+                           nominant_stats=nominant_stats, nominant_details=nominant_details)
+
 
 @app.route('/api/data')
 def api_data():
-    experts, nominants, votes, nominant_stats = get_data()
-    return jsonify(experts=experts, nominants=nominants, votes=votes, nominant_stats=nominant_stats)
+    experts, nominants, votes, nominant_stats, nominant_details = get_data()
+    return jsonify(experts=experts, nominants=nominants, votes=votes, 
+                   nominant_stats=nominant_stats, nominant_details=nominant_details)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
