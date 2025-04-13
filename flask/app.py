c86472e7fbac23127b9fe5537fac431fb7ea40b2
Дмитрий Коротков <dmitrii.korotkov@lemanapro.ru>
2025-04-13 19:55:25 +0300

    Теперь реальные названия

--- flask/app.py
+++ flask/app.py
@@ -4,7 +4,6 @@ import statistics
 
 app = Flask(__name__)
 
-# Функция для получения данных и вычисления средней оценки и дисперсии
 def get_data():
     db_path = r'C:\YandexDisk\! Stuff\VIP-Pelmeni\voting.db'
     conn = sqlite3.connect(db_path)
@@ -14,32 +13,30 @@ def get_data():
     cursor.execute("SELECT id, name FROM experts")
     experts = cursor.fetchall()
 
-    # Извлекаем всех номинантов
-    cursor.execute("SELECT id, name FROM nominants")
+    # Извлекаем всех номинантов (secret_name вместо name)
+    cursor.execute("SELECT id, secret_name FROM nominants")
     nominants = cursor.fetchall()
 
     # Извлекаем все оценки
     cursor.execute("SELECT expert_id, nominant_id, rating FROM votes")
     votes = cursor.fetchall()
 
-    # Создаем словари для хранения оценок по номинантам
+    # Создаем словарь для хранения оценок по номинантам
     nominant_ratings = {nominant[0]: [] for nominant in nominants}
-    
     for vote in votes:
         nominant_ratings[vote[1]].append(vote[2])
 
     # Вычисляем среднюю оценку и дисперсию для каждого номинанта
     nominant_stats = {}
     for nominant_id, ratings in nominant_ratings.items():
-        if ratings:  # если есть хотя бы одна оценка
-            avg_rating = sum(ratings) / len(ratings)  # средняя оценка
-            variance = statistics.variance(ratings) if len(ratings) > 1 else 0  # дисперсия
+        if ratings:
+            avg_rating = sum(ratings) / len(ratings)
+            variance = statistics.variance(ratings) if len(ratings) > 1 else 0
             nominant_stats[nominant_id] = {'avg': avg_rating, 'variance': variance}
         else:
             nominant_stats[nominant_id] = {'avg': 0, 'variance': 0}
-    
-    conn.close()
 
+    conn.close()
     return experts, nominants, votes, nominant_stats
 
 @app.route('/')
